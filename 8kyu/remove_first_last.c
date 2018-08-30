6062a46cbf36706ede41bf61e4cdc6644d9f05c6
Leexiang <NeonLeexiang@users.noreply.github.com>
2018-08-30 18:17:08 +0800

    Update remove_first_last.c

--- 8kyu/remove_first_last.c
+++ 8kyu/remove_first_last.c
@@ -6,3 +6,13 @@ char* remove_char(char* dst, const char* src)
     dst[strlen(dst) - 1] = '\0';
     return dst;
 }
+
+
+/*
+char *remove_char(char* dst, const char* src) {
+    src ++;
+    strncpy(dst, src, strlen(src)-1);
+    dst[strlen(src)-1] = '\0';
+    return dst;
+}
+*/
