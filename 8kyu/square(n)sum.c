4950b672de9895cc28012b61b9b9631fb42ec686
Leexiang <NeonLeexiang@users.noreply.github.com>
2018-08-30 17:32:34 +0800

    Update square(n)sum.c

--- 8kyu/square(n)sum.c
+++ 8kyu/square(n)sum.c
@@ -10,3 +10,17 @@ int square_sum(const int *values, size_t count){
     }
     return sums;
 }
+
+
+/*
+#include <stddef.h>
+#include<math.h>
+
+square_sum(const int *values, size_t count){
+  int sum = 0;
+  for(int i =0; i<count; i++)
+    sum+=pow(values[i], 2);
+  return sum;
+}
+int square_sum(const int *values, size_t count);
+*/
