89994012856beeae14ad54d1418bacef0d362e8f
Leexiang <NeonLeexiang@users.noreply.github.com>
2018-10-21 16:28:07 +0800

    Update redacted.c

--- 7kyu/redacted.c
+++ 7kyu/redacted.c
@@ -11,3 +11,16 @@ bool redacted(const char *doc1, const char *doc2)
   }
   return !*doc1 && !*doc2;
 }
+
+
+/*
+#include <stdbool.h>
+
+bool redacted(const char *doc1, const char *doc2)
+{
+    for (; *doc1 && *doc2; doc1++, doc2++)
+        if ( *doc1 != *doc2 && ('X' != *doc1 || '\n' == *doc2) )
+            return false;
+    return *doc1 == *doc2;
+}
+*/
